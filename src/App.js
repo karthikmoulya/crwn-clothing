b63770d96e9c7398c23cecb84aa483356981db0c
Karthik Moulya <karthik@pop-os.localdomain>
2020-08-15 08:33:49 +0530

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/ShopPage';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
